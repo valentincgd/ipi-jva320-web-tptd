dae01bc32425210e7acb6a897e79cea16f82e6bd
valentincgd <hypetofarstreet@gmail.com>
2023-04-18 00:57:45 +0200

    Update

--- src/main/java/com/ipi/jva320/controller/MainController.java
+++ src/main/java/com/ipi/jva320/controller/MainController.java
@@ -82,7 +82,6 @@ public class MainController {
 
         SalarieAideADomicile aide = new SalarieAideADomicile(nom, LocalDate.parse(moisEnCours), LocalDate.parse(moisDebutContrat), joursTravaillesAnneeN, congesPayesAcquisAnneeN, joursTravaillesAnneeNMoins1, congesPayesAcquisAnneeNMoins1, congesPayesPrisAnneeNMoins1);
         salarieAideADomicileService.creerSalarieAideADomicile(aide);
-
         return "redirect:/salaries/" + aide.getId();
     }
 
@@ -94,18 +93,50 @@ public class MainController {
         return "redirect:/salaries/" + (id-1);
     }
 
+
     @GetMapping("/salaries")
-    public String rechercherSalarie(@RequestParam("matricule") String nom) throws SalarieException {
+    public String ListSalaries(Model model, @RequestParam(value = "matricule",required = false) String nom, @RequestParam(value = "page",required = false,defaultValue = "0") String page,@RequestParam(value = "size",required = false,defaultValue = "5") String size) throws SalarieException {
 
-        List<SalarieAideADomicile> aide = salarieAideADomicileService.getSalaries(nom);
 
-        if(aide.stream().count() == 0){
-            return "404";
+
+        if(nom != null){
+            List<SalarieAideADomicile> aide = salarieAideADomicileService.getSalaries(nom);
+            if(aide.stream().count() == 0){
+                return "404";
+            }
+            return "redirect:/salaries/" + aide.get(0).getId();
         }
 
+        if(page == null){
+            page = "0";
+        }
 
-        return "redirect:/salaries/" + aide.get(0).getId();
+        if(size == null){
+            size = "5";
+        }
+
+        List<SalarieAideADomicile> users = salarieAideADomicileService.getSalaries();
+
+        model.addAttribute("users",getPage(users,Integer.parseInt(page),Integer.parseInt(size)));
+        model.addAttribute("size",Integer.parseInt(size));
+        model.addAttribute("pageN",Integer.parseInt(page));
+        model.addAttribute("NextPage",Integer.parseInt(page)+1);
+        model.addAttribute("BackPage",Integer.parseInt(page)-1);
+        model.addAttribute("totalSalarie",users.stream().count());
+
+
+
+
+        return "list";
     }
+
+
+    public static List<SalarieAideADomicile> getPage(List<SalarieAideADomicile> list, int page, int elementsPerPage) {
+        int startIndex = page * elementsPerPage;
+        int endIndex = Math.min(startIndex + elementsPerPage, list.size());
+        return list.subList(startIndex, endIndex);
+    }
+
 
 
 
